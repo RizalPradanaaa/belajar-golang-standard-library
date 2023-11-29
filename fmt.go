be61d7baf96d8063ed8d39abdb71ec654bb2f2f4
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2023-11-29 11:08:02 +0700

    Package Fmt

---
+++ fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+	firstname := "Rizal"
+	lastname := "Pradana"
+
+	fmt.Printf("Hello %s %s! \n", firstname, lastname)
+}
